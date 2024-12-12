592eeb16f8159824c163e3f6bbbd5cdbfb54df86
BugHunter-X <askzenai@gmail.com>
2024-12-12 14:50:06 +0100

    Add DimensionsUndefined.js file

---
+++ DimensionsUndefined.js
@@ -0,0 +1,1 @@
+This error occurs when using the `Dimensions` API in React Native to get screen dimensions, but the dimensions are not immediately available when the component mounts.  This often results in `undefined` values for `window.width` and `window.height`.
